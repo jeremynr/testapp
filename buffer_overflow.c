4efc0a15068420a87c3aeca67b5ae63b284521f3
jeremynr <1068730+jeremynr@users.noreply.github.com>
2018-08-24 05:45:12 -0500

    updated buffer_overflow.c to a more detailed buffer overflow

--- buffer_overflow.c
+++ buffer_overflow.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-	char str1[10];
-	char str2[] = "abcdefghijklmn";
-	strcpy(str1,str2);
+int main(int argc, char *argv[]){
+
+	char buffer[5];
+
+	if(argc<2){
+		printf("strcpy() aborted...\n");
+		printf("usage: %s <characters>\n", argv[0]);
+	}
+
+	strcpy(buffer, argv[1]);
+	printf("buffer content= %s \n",buffer);
+	printf("strcpy() executed");
+
+	return 0;
+
 }
+
